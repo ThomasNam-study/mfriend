fa0bbfc5b7376c5a85f4bd7a70668e17b63148d5
RED <purred@naver.com>
2020-04-09 08:59:51 +0900

    JUINT 5 까지 적용

---
+++ src/main/java/kr/purred/fc/mfriend/MfriendApplication.java
@@ -0,0 +1,13 @@
+package kr.purred.fc.mfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfriendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/purred/fc/mfriend/MfriendApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.purred.fc.mfriend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MfriendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
