9d8c62838297323c1cffb8c81d54f1cda4d707c3
RED <purred@naver.com>
2020-04-17 09:37:30 +0900

    Block 객체 적용중

---
+++ src/main/java/kr/purred/fc/mfriend/repository/BlockRepository.java
@@ -0,0 +1,8 @@
+package kr.purred.fc.mfriend.repository;
+
+import kr.purred.fc.mfriend.domain.Block;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlockRepository extends JpaRepository<Block, Long>
+{
+}
---
+++ src/test/java/kr/purred/fc/mfriend/service/PersonServiceTest.java
@@ -0,0 +1,18 @@
+package kr.purred.fc.mfriend.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonServiceTest
+{
+	@Autowired
+	private PersonService personService;
+
+	@Test
+	void getPeopleExcludeBlocks ()
+	{
+
+	}
+}
