b0709ffe6ac39c97c67c46e4fe653f8d3f3954d5
RED <purred@naver.com>
2020-04-21 13:48:13 +0900

    게시판 리스트작업 중

--- src/main/java/kr/purred/fc/mfriend/MfriendApplication.java
+++ src/main/java/kr/purred/fc/mfriend/MfriendApplication.java
@@ -1,7 +1,17 @@
 package kr.purred.fc.mfriend;
 
+import kr.purred.fc.mfriend.domain.Board;
+import kr.purred.fc.mfriend.domain.User;
+import kr.purred.fc.mfriend.enums.BoardType;
+import kr.purred.fc.mfriend.repository.BoardRepository;
+import kr.purred.fc.mfriend.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class MfriendApplication {
@@ -10,4 +20,23 @@ public class MfriendApplication {
 		SpringApplication.run(MfriendApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository)
+	{
+		return (args) -> {
+			User user = userRepository.save (User.builder ().name ("havi").password ("test").email ("purred@gmail.com").createdDate (LocalDateTime.now ()).build ());
+
+			IntStream.rangeClosed (1, 200).forEach ((index) -> {
+				boardRepository.save (Board.builder ()
+						.title ("게시글" + index)
+						.subTitle ("순서" + index)
+						.content ("내용")
+						.boardType (BoardType.free)
+						.createdDate (LocalDateTime.now ())
+						.updatedDate (LocalDateTime.now ())
+						.user (user)
+						.build ());
+			});
+		};
+	}
 }
---
+++ src/main/java/kr/purred/fc/mfriend/controller/BoardController.java
@@ -0,0 +1,36 @@
+package kr.purred.fc.mfriend.controller;
+
+import kr.purred.fc.mfriend.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController
+{
+	@Autowired
+	BoardService boardService;
+
+	@GetMapping({"", "/"})
+	public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model)
+	{
+		model.addAttribute ("board", boardService.findBoardByIdx (idx));
+
+		return "/board/form";
+	}
+
+	@GetMapping("/list")
+	public String list(@PageableDefault  Pageable pageable, Model model)
+	{
+		model.addAttribute ("boardList", boardService.findBoardList (pageable));
+
+		return "/board/list";
+	}
+}
