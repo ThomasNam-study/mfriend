a75c23238688436acaa3f43545d9f6f8e2a4c56f
ThomasNam <purred@naver.com>
2020-04-21 22:21:37 +0900

    Repository Test

--- src/test/java/kr/purred/fc/mfriend/repository/PersonRepositoryTest.java
+++ src/test/java/kr/purred/fc/mfriend/repository/PersonRepositoryTest.java
@@ -1,16 +1,19 @@
 package kr.purred.fc.mfriend.repository;
 
 import kr.purred.fc.mfriend.domain.Person;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Transactional
 @SpringBootTest
 class PersonRepositoryTest
 {
@@ -18,6 +21,22 @@ class PersonRepositoryTest
 	PersonRepository personRepository;
 
 	@Test
+	void findByName ()
+	{
+		List<Person> people = personRepository.findByName("tony");
+
+		assertThat(people.size ()).isEqualTo(1);
+
+		Person person = people.get(0);
+
+		assertAll(
+				() -> Assertions.assertThat(person.getName ()).isEqualTo("tony"),
+				() -> Assertions.assertThat(person.getHobby ()).isEqualTo("reading"),
+				() -> Assertions.assertThat(person.getAddress ()).isEqualTo("Seoul")
+		);
+	}
+
+	/*@Test
 	@Disabled
 	void crud ()
 	{
@@ -33,7 +52,7 @@ class PersonRepositoryTest
 		assertThat(persons.size ()).isEqualTo (1);
 		assertThat(persons.get (0).getName ()).isEqualTo ("john");
 		assertThat(persons.get (0).getAge ()).isEqualTo (20);
-	}
+	}*/
 
 	/*@Test
 	void hashTest ()
--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -3,6 +3,8 @@ insert into person(`id`, `name`, `year`, `month`, `day`) values(2, 'david', 1994
 insert into person(`id`, `name`, `year`, `month`, `day`) values(3, 'dennis', 1998, 7, 21);
 insert into person(`id`, `name`, `year`, `month`, `day`) values(4, 'sophia', 2000, 8, 10);
 insert into person(`id`, `name`, `year`, `month`, `day`) values(5, 'benny',  1982, 2, 16);
+insert into person(`id`, `name`, `year`, `month`, `day`, `job`, `hobby`, `phone_number`, `address`) values(6, 'tony',  1991, 7, 10, 'officer', 'reading', '010-2222-2222', 'Seoul');
+insert into person(`id`, `name`, `deleted`) values(7, 'andy',  1);
 
 /*insert into block(`id`, `name`) values (1, 'dennis');
 insert into block(`id`, `name`) values (2, 'sophia');
