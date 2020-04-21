85da2bda3dccd5f587471a24dbde1b39644252ed
ThomasNam <purred@naver.com>
2020-04-21 23:21:44 +0900

    Service 테스트 Mockito 적용

--- src/test/java/kr/purred/fc/mfriend/service/PersonServiceTest.java
+++ src/test/java/kr/purred/fc/mfriend/service/PersonServiceTest.java
@@ -3,26 +3,82 @@ package kr.purred.fc.mfriend.service;
 import kr.purred.fc.mfriend.domain.Block;
 import kr.purred.fc.mfriend.domain.Person;
 import kr.purred.fc.mfriend.domain.dto.Birthday;
+import kr.purred.fc.mfriend.dto.PersonDto;
 import kr.purred.fc.mfriend.repository.BlockRepository;
 import kr.purred.fc.mfriend.repository.PersonRepository;
 import net.bytebuddy.asm.Advice;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
+// @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class PersonServiceTest
 {
-	@Autowired
+	/**
+	 * 테스트 대상이 되는 클래스
+	 */
+	// @Autowired
+	@InjectMocks
 	private PersonService personService;
 
-	@Autowired
+	// @Autowired
+	@Mock
 	private PersonRepository personRepository;
+
+	@Test
+	void getPersonTest ()
+	{
+		when(personRepository.findById(1L))
+				.thenReturn(Optional.of(new Person("martin")));
+
+		Person person = personService.getPerson(1L);
+
+		assertThat(person.getName()).isEqualTo("martin");
+	}
+
+	@Test
+	void getPersonIfNotFound ()
+	{
+		when(personRepository.findById(1L))
+				.thenReturn(Optional.empty());
+
+		Person person = personService.getPerson(1L);
+
+		assertThat(person).isNull();
+	}
+
+	@Test
+	void put ()
+	{
+		PersonDto dto = new PersonDto();
+
+		dto.setName("martin");
+		dto.setBirthday(LocalDate.now());
+		dto.setAddress("Seoul");
+		dto.setHobby("Programming");
+		dto.setPhoneNumber("010-5349-6254");
+
+		personService.addPerson(dto);
+
+		verify(personRepository, times(1)).save(any(Person.class));
+	}
+
+
 
 	@Test
 	void birthdayTest ()
@@ -44,9 +100,13 @@ class PersonServiceTest
 	@Test
 	void getPeopleByName ()
 	{
+		when(personRepository.findByName("martin"))
+				.thenReturn(Lists.newArrayList(new Person("martin")));
+
 		List<Person> lists = personService.getPeopleByName ("martin");
 
-		lists.stream ().forEach (System.out::println);
+		assertThat (lists.size()).isEqualTo(1);
+		assertThat (lists.get(0).getName ()).isEqualTo("martin");
 	}
 
 	/*@Test
@@ -84,6 +144,7 @@ class PersonServiceTest
 		Person person = personService.getPerson (3L);
 
 		assertThat(person).isNotNull ();
+		assertThat(person).isEqualTo ("dennis");
 	}
 
 	/*private void givenPeople ()
